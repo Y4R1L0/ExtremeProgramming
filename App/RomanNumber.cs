1c44b2bf5809c7df8a3fbb8a9a40c2d383f697d5
Yaroslav <Yaroslav@DESKTOP-QAESUSL>
2023-09-06 11:56:33 +0300

    RomanNumbers2

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -8,13 +8,45 @@ namespace App
 {
     public class RomanNumber
     {
-        public Int32 Value { get; set; }
-        public static RomanNumber Parse(String input)
+        public int Value { get; set; }
+
+        private static Dictionary<char, int> romanValues = new Dictionary<char, int>
         {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static RomanNumber Parse(string roman)
+        {
+            int result = 0;
+            int prev = 0;
+
+            for (int i = roman.Length - 1; i >= 0; i--)
+            {
+                int current = romanValues[roman[i]];
+
+                if (current < prev)
+                {
+                    result -= current;
+                }
+                else
+                {
+                    result += current;
+                }
+
+                prev = current;
+            }
+
             return new()
             {
-                Value = input.Length
+                Value = result
             };
         }
+
     }
 }
